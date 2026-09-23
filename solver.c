c69ce79f41354453601811d30b06f75b136633f7
Shea McCall <sdm253@nau.edu>
2026-09-23 22:32:13 +0000

    All solutions

--- solver.c
+++ solver.c
@@ -1,10 +1,8 @@
-#include "nqueens.h"
-
 // Solve the N-Queens problem recursively
 bool solve_nqueens_util(int board[], int col, int n) {
     if (col >= n) {
         print_board(board, n); // Print the current solution
-        return true; 
+        return false; 
     }
 
     for (int i = 0; i < n; i++) {
